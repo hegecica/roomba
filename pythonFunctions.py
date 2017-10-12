8df4d88267187a20b7778c01e66a16683787c807
Tamas Hegedus <hegedus.tamas15@gmail.com>
2017-10-12 12:05:15 +0200

    Rem: print; Add: Fwd, stop

--- pythonFunctions.py
+++ pythonFunctions.py
@@ -1,13 +1,27 @@
 # This file contains our functions for the robocar
+import gpioPinout
 
 
-def printSomeText():
-    print('Just testing...')
-    return True
+def fwdcycle():
+    GPIO.output(ain1, GPIO.LOW)
+    GPIO.output(ain2, GPIO.HIGH)
+    GPIO.output(bin1, GPIO.LOW)
+    GPIO.output(bin2, GPIO.HIGH)
+    leftmotor.start(0)
+    rightmotor.start(0)
+    leftmotor.ChangeDutyCycle(40)
+    rightmotor.ChangeDutyCycle(40)
+
+
+def stop():
+    rightmotor.stop(0)
+    leftmotor.stop(0)
 
 
 def main():
-    printSomeText()
+    fwdcycle()
+    time.sleep(1)
+    stop()
 
 
 main()
