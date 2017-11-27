54df27a10aaf6267826e719eb5da7c1de09c58c3
Tamas Hegedus <hegedus.tamas15@gmail.com>
2017-11-27 09:47:35 +0100

    Clearing up - No output

--- lineFollower.py
+++ lineFollower.py
@@ -20,31 +20,22 @@ def main():
         csens = GPIO.input(cent)
         rsens = GPIO.input(rght)
 
-        print(lsens, csens, rsens)
-
         if lsens == 0 and csens == 0 and rsens == 0:
             roomba.moveBackward(100)
-            print("BCK")
         elif lsens == 0 and csens == 0 and rsens == 1:
             roomba.turnRight(30)
-            print("RGHT")
         elif lsens == 0 and csens == 1 and rsens == 0:
             roomba.moveForward(100)
-            print("FWD")
         elif lsens == 0 and csens == 1 and rsens == 1:
             roomba.softRight(25)
-            print("SRGHT")
         elif lsens == 1 and csens == 0 and rsens == 0:
             roomba.turnLeft(30)
-            print("LEFT")
         elif lsens == 1 and csens == 0 and rsens == 1:
             roomba.stop()
         elif lsens == 1 and csens == 1 and rsens == 0:
             roomba.softLeft(25)
-            print("SLEFT")
         elif lsens == 1 and csens == 1 and rsens == 1:
             roomba.moveForward(100)
-            print("FWD")
 
 
 main()
