d84b52e2751ffa4fa3a46a9b69f71a53770842d5
Tamas Hegedus <hegedus.tamas15@gmail.com>
2017-11-27 09:53:23 +0100

    Roomba library

--- keyboardControl.py
+++ keyboardControl.py
@@ -2,85 +2,7 @@ import RPi.GPIO as GPIO
 import sys
 import termios
 import tty
-# import time
-# Pins for the LEFT motor
-ain1 = 17
-ain2 = 27
-pwma = 22
-# Pins for the RGHT motor
-bin1 = 16
-bin2 = 20
-pwmb = 21
-# Pins for the LIGHT sensors
-left = 10
-cent = 9
-rght = 11
-# GPIO general setup
-GPIO.setwarnings(True)
-GPIO.setmode(GPIO.BCM)
-# GPIO setup for LEFT motor
-GPIO.setup(ain1, GPIO.OUT)
-GPIO.setup(ain2, GPIO.OUT)
-GPIO.setup(pwma, GPIO.OUT)
-leftmotor = GPIO.PWM(pwma, 50)
-# GPIO setup for RGHT motor
-GPIO.setup(bin1, GPIO.OUT)
-GPIO.setup(bin2, GPIO.OUT)
-GPIO.setup(pwmb, GPIO.OUT)
-rightmotor = GPIO.PWM(pwmb, 50)
-# GPIO setup for LIGHT sensors
-GPIO.setup(left, GPIO.IN)
-GPIO.setup(cent, GPIO.IN)
-GPIO.setup(rght, GPIO.IN)
-
-
-def fwdcycle():
-    GPIO.output(ain1, GPIO.HIGH)
-    GPIO.output(ain2, GPIO.LOW)
-    GPIO.output(bin1, GPIO.HIGH)
-    GPIO.output(bin2, GPIO.LOW)
-    leftmotor.start(0)
-    rightmotor.start(0)
-    leftmotor.ChangeDutyCycle(70)
-    rightmotor.ChangeDutyCycle(70)
-
-
-def backward():
-    GPIO.output(ain1, GPIO.LOW)
-    GPIO.output(ain2, GPIO.HIGH)
-    GPIO.output(bin1, GPIO.LOW)
-    GPIO.output(bin2, GPIO.HIGH)
-    leftmotor.start(0)
-    rightmotor.start(0)
-    leftmotor.ChangeDutyCycle(70)
-    rightmotor.ChangeDutyCycle(70)
-
-
-def left():
-    GPIO.output(ain1, GPIO.LOW)
-    GPIO.output(ain2, GPIO.HIGH)
-    GPIO.output(bin1, GPIO.HIGH)
-    GPIO.output(bin2, GPIO.LOW)
-    leftmotor.start(0)
-    rightmotor.start(0)
-    leftmotor.ChangeDutyCycle(30)
-    rightmotor.ChangeDutyCycle(30)
-
-
-def right():
-    GPIO.output(ain1, GPIO.HIGH)
-    GPIO.output(ain2, GPIO.LOW)
-    GPIO.output(bin1, GPIO.LOW)
-    GPIO.output(bin2, GPIO.HIGH)
-    leftmotor.start(0)
-    rightmotor.start(0)
-    leftmotor.ChangeDutyCycle(30)
-    rightmotor.ChangeDutyCycle(30)
-
-
-def stop():
-    rightmotor.stop(0)
-    leftmotor.stop(0)
+import roomba
 
 
 def getch():
@@ -99,18 +21,20 @@ def main():
     while direction != "\x03":
         direction = getch()
         if direction == 'w':
-            fwdcycle()
+            roomba.moveForward(100)
         elif direction == 's':
-            backward()
+            roomba.moveBackward(100)
         elif direction == 'a':
-            left()
+            roomba.turnLeft(100)
         elif direction == 'd':
-            right()
+            roomba.turnRight(100)
         elif direction == 'x':
-            stop()
+            roomba.stop()
+        elif direction == "\x03":
+            print('Exiting.. Bye!')
         else:
             print('INVALID:', direction)
-    stop()
+    roomba.stop()
 
 
 main()
