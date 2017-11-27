339096972c11d686ead23c61c0e017002bb1de67
Tamas Hegedus <hegedus.tamas15@gmail.com>
2017-11-27 12:47:22 +0100

    Speed

--- lineFollower.py
+++ lineFollower.py
@@ -21,21 +21,21 @@ def main():
         rsens = GPIO.input(rght)
 
         if lsens == 0 and csens == 0 and rsens == 0:
-            roomba.moveBackward(55)
+            roomba.moveBackward(20)
         elif lsens == 0 and csens == 0 and rsens == 1:
-            roomba.turnRight(30)
+            roomba.turnRight(5)
         elif lsens == 0 and csens == 1 and rsens == 0:
-            roomba.moveForward(75)
+            roomba.moveForward(20)
         elif lsens == 0 and csens == 1 and rsens == 1:
-            roomba.softRight(25)
+            roomba.softRight(5)
         elif lsens == 1 and csens == 0 and rsens == 0:
-            roomba.turnLeft(30)
+            roomba.turnLeft(5)
         elif lsens == 1 and csens == 0 and rsens == 1:
             roomba.stop()
         elif lsens == 1 and csens == 1 and rsens == 0:
-            roomba.softLeft(25)
+            roomba.softLeft(5)
         elif lsens == 1 and csens == 1 and rsens == 1:
-            roomba.moveForward(75)
+            roomba.moveForward(20)
 
 
 main()
